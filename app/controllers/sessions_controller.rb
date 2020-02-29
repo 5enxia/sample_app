312b6d23ce085f353aa597d57fcb91ccd929ad35
takayasumorishita <c011729632@edu.teu.ac.jp>
2020-02-29 19:28:38 +0900

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,21 +2,22 @@ class SessionsController < ApplicationController
   def new
   end
 
+  # セッションを生成
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      redirect_to user
+      log_in user # ログイン処理
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_to user  # ユーザページへリダイレクト
     else
       flash.now[:danger] = 'Invalid email/password combination' 
       render 'new'
     end
   end
 
-
+  # セッションを破棄する
   def destroy
-    log_out
+    log_out if logged_in? # ログインしていれば，log_outを呼び出す
     redirect_to root_url
   end
-
 end
