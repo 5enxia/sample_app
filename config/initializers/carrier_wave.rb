a25a06130d559ad2d3ed63a0350b11bfd2a82312
takayasumorishita <c011729632@edu.teu.ac.jp>
2020-04-06 18:45:16 +0900

    comment out fog host&endpont

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -7,11 +7,11 @@ if Rails.env.production?
         :aws_secret_access_key => ENV['CLOUDCUBE_SECRET_ACCESS_KEY'],
         :region                => ENV['CLOUDCUBE_REGION'],
         # :host                  => ENV['CLOUDCUBE_HOST'],
-        :endpoint              => ENV['CLOUDCUBE_URL'] 
+        # :endpoint              => ENV['CLOUDCUBE_URL'] 
       }
       
       config.fog_directory     =  ENV['CLOUDCUBE_BUCKET']
       config.fog_public        =  false
-
+x
     end
   end
