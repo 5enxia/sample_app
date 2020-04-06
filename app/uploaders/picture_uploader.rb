4d10fb74763d30cc03e3b050f1f8bac27397b596
takayasumorishita <c011729632@edu.teu.ac.jp>
2020-04-06 19:09:20 +0900

    add cache_dir and change store_dir

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -15,8 +15,13 @@ class PictureUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   # アップロードファイルの保存先ディレクトリ
+
+  def cache_dir
+    "ud6eqvaic4yl/uploads/tmp"
+  end
+
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "ud6eqvaic4yl/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -6,8 +6,6 @@ if Rails.env.production?
         :aws_access_key_id     => ENV['CLOUDCUBE_ACCESS_KEY_ID'],
         :aws_secret_access_key => ENV['CLOUDCUBE_SECRET_ACCESS_KEY'],
         :region                => ENV['CLOUDCUBE_REGION'],
-        # :host                  => ENV['CLOUDCUBE_HOST'],
-        # :endpoint              => ENV['CLOUDCUBE_URL'] 
       }
       
       config.fog_directory     =  ENV['CLOUDCUBE_BUCKET']
